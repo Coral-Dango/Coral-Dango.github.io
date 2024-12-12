6966f80652b30b1c79de2551a03f2aeaea7c4717
Coral <Trans.Coral.307@gmail.com>
2024-12-12 20:00:07 +0800

    Site updated: 2024-12-12 20:00:07

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1734004445345";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1734004790966";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
